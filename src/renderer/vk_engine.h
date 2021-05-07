6b2abd4be4a04356bb2ca9b3adb0505fe88196d3
Wysocki Patryk <wysockipawelpatryk@gmail.com>
2021-05-08 00:13:09 +0200

    scene management

--- src/renderer/vk_engine.h
+++ src/renderer/vk_engine.h
@@ -10,10 +10,25 @@
 #include <vector>
 #include <functional>
 #include <deque>
+#include <unordered_map>
+
 #include "vk_mesh.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
+
+struct Material {
+	VkPipeline pipeline;
+	VkPipelineLayout pipelineLayout;
+};
+
+struct RenderObject {
+	Mesh* mesh;
+
+	Material* material;
+
+	glm::mat4 transformMatrix;
+};
 
 struct MeshPushConstants {
 	glm::vec4 data;
@@ -131,6 +146,25 @@ public:
 	//the format for the depth image
 	VkFormat _depthFormat;
 
+	//default array of renderable objects
+	std::vector<RenderObject> _renderables;
+
+	std::unordered_map<std::string,Material> _materials;
+	std::unordered_map<std::string,Mesh> _meshes;
+
+	//functions
+	//create material and add it to the map
+	Material* create_material(VkPipeline pipeline, VkPipelineLayout layout, const std::string& name);
+
+	//returns nullptr if it can't be found
+	Material* get_material(const std::string& name);
+
+	//returns nullptr if it can't be found
+	Mesh* get_mesh(const std::string& name);
+
+	//our draw function
+	void draw_objects(VkCommandBuffer cmd,RenderObject* first, int count);
+
 	int _selectedShader{ 0 };
 
 private:
@@ -143,6 +177,7 @@ private:
 	void init_pipelines();
 
 	void load_meshes();
+	void init_scene();
 
 	void upload_mesh(Mesh& mesh);
 };
